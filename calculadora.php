58ea66be452b841e025ec003a44bb1966938b9c2
zhenhao <zyu@inscamidemar.cat>
2026-03-10 16:16:37 +0100

    Afegida funció calcular() amb la lògica de càlcul

--- calculadora.php
+++ calculadora.php
@@ -3,18 +3,19 @@ $operacio = $_POST['op'];
 $primerNumero = $_POST['n1'];
 $segonNumero = $_POST['n2'];
 
-
-if ($operacio == "s") {
-    $repuesta = $primerNumero + $segonNumero;
-} else if ($operacio == "r") {
-    $repuesta = $primerNumero - $segonNumero;
-} else if ($operacio == "m") {
-    $repuesta = $primerNumero * $segonNumero;
-} else if ($operacio == "d") {
-    if ($segonNumero != 0) {
-        $repuesta = $primerNumero / $segonNumero;
-    } else {
-        $repuesta = "Error";
+function calcular($operacio, $primerNumero, $segonNumero) {
+    if ($operacio == "s") {
+        return $primerNumero + $segonNumero;
+    } else if ($operacio == "r") {
+        return $primerNumero - $segonNumero;
+    } else if ($operacio == "m") {
+        return $primerNumero * $segonNumero;
+    } else if ($operacio == "d") {
+        if ($segonNumero != 0) {
+            return $primerNumero / $segonNumero;
+        } else {
+            return "Error";
+        }
     }
 }
 
